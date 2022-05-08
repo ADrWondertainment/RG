70ac226e577aa3bf016d85958ba364e924f95921
Icecream <pangxiaobo2017@outlook.com>
2022-05-09 00:58:14 +0800

    .

--- dao/src/main/java/ruangong/root/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package ruangong.root.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import ruangong.root.bean.User;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
