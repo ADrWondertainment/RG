c393d9cce81a79b12fcc47b00b277c9b5e19a711
Icecream <pangxiaobo2017@outlook.com>
2022-05-09 09:46:05 +0800

    commit

---
+++ bean/src/main/java/ruangong/root/bean/Template.java
@@ -0,0 +1,25 @@
+package ruangong.root.bean;
+
+import cn.hutool.json.JSONObject;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Scope("prototype")
+@Data
+@TableName("templates")
+public class Template {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+    private String description;
+    private String name;
+    private Date time;
+    private JSONObject data;
+    private Integer type;
+}
