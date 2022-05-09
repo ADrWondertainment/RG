04cc92d25892de47005b63ad49b76272f2ea0ce1
Icecream <pangxiaobo2017@outlook.com>
2022-05-09 18:17:55 +0800

    pxb

--- service_tao/src/main/java/ruangong/root/service_tao/UserService.java
+++
@@ -1,9 +0,0 @@
-package ruangong.root.service_tao;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import ruangong.root.bean.Result;
-import ruangong.root.bean.User;
-
-public interface UserService extends IService<User> {
-    public Result register(User user);
-}
