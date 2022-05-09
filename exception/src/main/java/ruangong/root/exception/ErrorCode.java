9c1df2d6da8ce19d3ee5edb8aae3a0741d1afca9
Icecream <pangxiaobo2017@outlook.com>
2022-05-09 23:39:26 +0800

    2022-5-9 23:39 update ErrorCode
    pxb

--- exception/src/main/java/ruangong/root/exception/ErrorCode.java
+++ exception/src/main/java/ruangong/root/exception/ErrorCode.java
@@ -1,6 +1,18 @@
 package ruangong.root.exception;
 
 public class ErrorCode {
+
+
+    public static final Integer USER_NAME_DUPLICATED = 10000;
+    public static final Integer USER_INSERT_FAILURE = 10010;
+    public static final Integer USER_INSERT_SUCCESS = 10020;
+    public static final Integer USER_NAME_UNFINDED = 10030;
+    public static final Integer USER_PASSWORD_UNMATCH = 10040;
+    public static final Integer USER_LOGIN_SUCCESS = 10050;
+
+    public static final Integer SUCCESS = 1;
+    public static final Integer FAIL = 0;
+
     public static final Integer ALL_SET = 66666;
 
     public static final Integer TEMPLATE_INSERT_SUCCESS = 20011;
