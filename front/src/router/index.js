afd41cef09b5720d77f08862d85e310d297c5e8f
LinYichen <Linjy15318955887@163.com>
2022-05-28 19:26:51 +0800

    登录逻辑完善

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -116,7 +116,7 @@ const router = createRouter({
 router.beforeEach((to, from) => {
     // console.log(to.path)
     // 已登录 || 要跳转到login || 要到signIn
-    if (sessionStorage["isLogin"] || to.path === '/login' || to.path === '/signIn') {
+    if (sessionStorage["isLogin"] || to.path === '/login' || to.path === '/signIn' || to.path === '/signInC') {
         return true
     }
     // else if (sessionStorage["isLogin"] && to.path != '/Login') {
