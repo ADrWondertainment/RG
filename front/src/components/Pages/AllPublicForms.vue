05e5e82cee66a16dd0ca4e7cc69947c9d65dc4f5
LinYichen <Linjy15318955887@163.com>
2022-06-21 17:24:45 +0800

    wssx

--- front/src/components/Pages/AllPublicForms.vue
+++ front/src/components/Pages/AllPublicForms.vue
@@ -66,7 +66,7 @@ export default {
   data() {
     return {
       formKind: "all",
-      returnList: null,
+      returnList: [],
       formStatus:[],
       testData: `{
     "errorCode": 66666,
