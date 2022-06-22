530e6f9b9b3b6d9a76592182182264b1b12ceff6
a_fake_name <1300327964@qq.com>
2022-06-22 12:56:55 +0800

    我就是林志康他爹

--- front/src/components/Pages/DetailPM.vue
+++ front/src/components/Pages/DetailPM.vue
@@ -749,7 +749,7 @@
             console.log(res.data);
             console.log(Object.keys(res.data.data).length);
             for (var i = 0; i < Object.keys(res.data.data).length; i++) {
-                if(res.data.data.level==0) {
+                if(res.data.data[i].level==0) {
                     continue
                 }else{
                     this.FstaffFormInfo.push(res.data.data[i]);
