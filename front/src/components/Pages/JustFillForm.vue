872cbe2c285744ce15d1f9976f471143ebd910c7
LinYichen <Linjy15318955887@163.com>
2022-06-22 12:44:20 +0800

    test

--- front/src/components/Pages/JustFillForm.vue
+++ front/src/components/Pages/JustFillForm.vue
@@ -215,7 +215,10 @@ export default {
     } else if (this.$route.params.ShowResult === "approve") {
       console.log(this.$route.params.ShowResult);
       axios
-        .post("/api/answers/pre/" + this.$route.params.FormId, {})
+        .post("/api/answers/pre/", {
+          sheetId: this.$route.params.FormId,
+          answers: null,
+        })
         .then((res) => {
           console.log(res.data.data);
           if (res.data.errorCode == 66666) {
@@ -267,7 +270,10 @@ export default {
       console.log("url填写");
       console.log(this.$route.params.ShowResult);
       axios
-        .post("/api/answers/pre/" + this.$route.params.FormId, {})
+        .post("/api/answers/pre/", {
+          sheetId: this.$route.params.FormId,
+          answers: null,
+        })
         .then((res) => {
           console.log(res.data.data);
           if (res.data.errorCode == 66666) {
